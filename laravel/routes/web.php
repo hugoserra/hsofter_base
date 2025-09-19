9aa58ef6694310d6d185505a8edea7af1074c53e
Hugo <tcarvalhotaichou@gmail.com>
2025-09-18 22:03:17 -0300

    first commit

---
+++ laravel/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('/', 'dashboard')->name('home');
+    Route::redirect('settings', 'settings/profile');
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
